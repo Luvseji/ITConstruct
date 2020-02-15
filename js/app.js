e9880393d82b60d596e3e714f814ef7d08648c41
Luvseji <49973630+Luvseji@users.noreply.github.com>
2020-02-15 21:06:46 +0700

    ver3.0

--- js/app.js
+++ js/app.js
@@ -5,43 +5,14 @@ $(function() {
     $("#navToggle").on("click", function(event) {
         nav.toggleClass("navigation__show");
         head.toggleClass("header__navi-on-click")
-        $("#catalog").removeClass("catalog__show");
-        $("#head-navi-wrap").removeClass("header__catlog-on-click");
-        $("#nav-cat").removeClass("catalog-bg");
-        $("#navigation__item-2").removeClass("cat-margin");
+        $(".sub-navigation__inner").removeClass("sub-navigation__show");
+        $(".sub-navigation__link").removeClass("sub-navigation__bg");
+        $(".sub-navigation").removeClass("sub-navigation__move");
     });
-
-    let cat = $("#catalog");
-    $("#nav-cat").on("click", function(event) {
-        cat.toggleClass("catalog__show");
-        $("#head-navi-wrap").toggleClass("header__catlog-on-click");
-        $("#nav-cat").toggleClass("catalog-bg");
-        $("#navigation__item-2").toggleClass("cat-margin");
-    });
-
-    /*Category border become green when hover*/
-    $("#cat-title-1").on("mouseover mouseout", function(event) {
-        $("#cat-border-1").toggleClass("category__image_border-green");
-    });
-    $("#cat-title-2").on("mouseover mouseout", function(event) {
-        $("#cat-border-2").toggleClass("category__image_border-green");
-    });
-    $("#cat-title-3").on("mouseover mouseout", function(event) {
-        $("#cat-border-3").toggleClass("category__image_border-green");
-    });
-    $("#cat-title-4").on("mouseover mouseout", function(event) {
-        $("#cat-border-4").toggleClass("category__image_border-green");
-    });
-    $("#cat-title-5").on("mouseover mouseout", function(event) {
-        $("#cat-border-5").toggleClass("category__image_border-green");
-    });
-    $("#cat-title-6").on("mouseover mouseout", function(event) {
-        $("#cat-border-6").toggleClass("category__image_border-green");
-    });
-    $("#cat-title-7").on("mouseover mouseout", function(event) {
-        $("#cat-border-7").toggleClass("category__image_border-green");
-    });
-    $("#cat-title-8").on("mouseover mouseout", function(event) {
-        $("#cat-border-8").toggleClass("category__image_border-green");
+    /*Sub-navigation*/
+    $(".sub-navigation__link").on("click", function(event) {
+        $(this).next().toggleClass("sub-navigation__show");
+        $(this).toggleClass("sub-navigation__bg");
+        $(this).parent().toggleClass("sub-navigation__move");
     });
 });
