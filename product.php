589bf7b1ef84609559a6d5b1595a54c0864edf1d
Luvseji <49973630+Luvseji@users.noreply.github.com>
2020-05-20 20:08:15 +0700

    ver10.12

--- product.php
+++ product.php
@@ -4,6 +4,18 @@ require 'inc/connection.inc.php';
 require 'inc/functions.inc.php';
 require 'inc/init.inc.php';
 $product_id = ((int) $_GET['prod_id'] >= 0) ? ((int)$_GET['prod_id']) : 0;
+if (isset($_GET['cat_id'])) {
+    $cat_id = (int) $_GET['cat_id'];
+    $sql = "SELECT category_id FROM product_category_is WHERE product_id = $product_id";
+    if (!$result = mysqli_query($link, $sql)) {
+        header('Location: err404.php');
+    }
+    $cats_prod = mysqli_fetch_all($result, MYSQLI_NUM);
+    mysqli_free_result($result);
+    if (!in_array($cat_id, array_column($cats_prod, 0))) {
+        header('Location: err404.php');
+    }
+}
 $sql = "SELECT * FROM product WHERE id = $product_id";
 if (!$result = mysqli_query($link, $sql)) {
     header('Location: err404.php');
@@ -13,43 +25,26 @@ $product = $product[0];
 mysqli_free_result($result);
 if ($product) {
     $title = $product['name'];
+    if (!isset($cat_id)) {
+    $cat_id = $product['category_main_id'];
+    }
+    ob_start();
+    require 'inc/temp_head.inc.php';
+    $buffer = ob_get_contents();
+    ob_end_clean();
+    $buffer = preg_replace('/<!--#TITLE#-->/i', $title, $buffer);
+    if ($product && $cat_id != $product['category_main_id']) {
+        $buffer = preg_replace("<<!--#CANONICAL#-->>", "<link rel=\"canonical\" href=\"" . $_SERVER['REQUEST_URI'] . "\"/>", $buffer);
+    }
+    foreach ($categories as $key => $item) {
+        if ($item['id'] == $cat_id) {
+            $cat_id_key = $key;
+        }
+    }
 } else {
     $title = 'Извините, такого товара не существует';
 }
-ob_start();
-require 'inc/temp_head.inc.php';
-$buffer = ob_get_contents();
-ob_end_clean();
-$buffer = preg_replace('/<!--#TITLE#-->/i', $title, $buffer);
-if (isset($_GET['cat_id'])) {
-    $cat_id = (int) $_GET['cat_id'];
-    $sql = "SELECT category_id FROM product_category_is WHERE product_id = $product_id";
-    if (!$result = mysqli_query($link, $sql)) {
-        header('Location: err404.php');
-    }
-    $cats_prod = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    mysqli_free_result($result);
-    foreach ($cats_prod as $item) {
-        if ($item['category_id'] == $cat_id) {
-            $match_cat = true;
-        }
-    }
-    if (!$match_cat) {
-        header('Location: err404.php');
-    }
-    if ($cat_id != $product['category_main_id']) {
-        $buffer = preg_replace("<<!--#CANONICAL#-->>", "<link rel=\"canonical\" href=\"" . $_SERVER['REQUEST_URI'] . "\"/>", $buffer);
-    }
-
-} else {
-    $cat_id = $product['category_main_id'];
-}
 echo $buffer;
-foreach ($categories as $key => $item) {
-    if ($item['id'] == $cat_id) {
-        $cat_id_key = $key;
-    }
-}
 if ($product) : ?>
 <div class="path content__path">
     <ul class="path__inner">
